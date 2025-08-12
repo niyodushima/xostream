63305478261bd85ed6eadfcb38b9d2ae66deceb9
jered <niyodushimajered@gmail.com>
2025-08-12 09:03:06 -0700

    Switch to next.config.js wrapper

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
@@ -5,6 +6,12 @@ const nextConfig: NextConfig = {
   experimental: {
     appDir: true,
   },
+=======
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+>>>>>>> c491749 (Add next.config.js wrapper)
 };
 
 export default nextConfig;
